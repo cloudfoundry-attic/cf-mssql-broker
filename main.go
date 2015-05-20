6ea26716e0756c61ea616d30fceaf77491b2dc67
Stefan Schneider <stefan.schneider@hp.com>
2015-05-20 18:49:17 +0300

    Fail fast on panic
    Http server lib will recover from panics.
    This will just add a wrapper to the http request and create an additional
    goroutine.

--- main.go
+++ main.go
@@ -12,6 +12,26 @@ import (
 	"runtime"
 )
 
+type exitOnPanicWrapper struct {
+	parrent http.Handler
+}
+
+func (o exitOnPanicWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+
+		// Just wrap the ServeHTTP handler in a new goroutine
+		// so that panics thrown here will crash the process (i.e. fail fast).
+		// Some go sql drivers, like ODBC, have a lot process wide state
+		// and it may be very hard to recover from every internal failure.
+		// It is much safer for now to just crash the process and let SCM
+		// or other service manager restart the broker.
+		o.parrent.ServeHTTP(w, r)
+	}()
+	<-done
+}
+
 const (
 	DEBUG = "debug"
 	INFO  = "info"
@@ -97,8 +117,8 @@ func runMain() {
 	addr := getListeningAddr(brokerConfig)
 	logger.Info("start-listening", lager.Data{"addr": addr})
 
-	err = http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, exitOnPanicWrapper{http.DefaultServeMux})
 	if err != nil {
-		logger.Error("error-listenting", err)
+		logger.Fatal("error-listening", err)
 	}
 }
